bbca8bc18d2946c3fed721042e65e4e3e89048a2
Ilaria Salvati <ilariasalvati00@gmail.com>
2024-01-31 16:48:06 +0100

    rimesse dipendenze MovieSeeder

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Movie;
+use Carbon\Carbon;
 
 class MovieSeeder extends Seeder
 {
