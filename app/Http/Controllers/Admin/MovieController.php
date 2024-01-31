41f1ec3e10677e24bf6e7cdbc27b2a0c6ad64ef3
Ilaria Salvati <ilariasalvati00@gmail.com>
2024-01-31 19:03:45 +0100

    edit fatta

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -46,7 +46,7 @@ class MovieController extends Controller
      */
     public function show(Movie $movie)
     {
-        //
+        return view("admin.movies.show", compact("movie"));
     }
 
     /**
