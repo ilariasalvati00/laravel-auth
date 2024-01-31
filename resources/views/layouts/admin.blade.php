1b46e4787aa781c033db03240f0c81888a1aeac3
Ilaria Salvati <ilariasalvati00@gmail.com>
2024-01-31 15:29:56 +0100

    modificato admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,48 +1,68 @@
-@vite(['resources/js/app.js'])
- </head>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
- <body>
- 	<div id="app">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
- 		<div class="container-fluid vh-100">
- 			<div class="row h-100">
- 				<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
- 					<div class="position-sticky pt-3">
- 						<ul class="nav flex-column">
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
- 							<li class="nav-item">
- 								<a class="nav-link text-white" href="/">
- 									<i class="fa-solid fa-home-alt fa-lg fa-fw"></i> Home
- 								</a>
- 							</li>
+	<title>{{ config('app.name', 'Laravel') }}</title>
 
- 							<li class="nav-item">
- 								<a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}" href="{{route('admin.dashboard')}}">
- 									<i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
- 								</a>
- 							</li>
+	<!-- Fontawesome 6 cdn -->
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
 
- 							<li class="nav-item">
- 								<a class="nav-link text-white" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
- 									<i class="fa-solid fa-sign-out-alt fa-lg fa-fw"></i> {{ __('Logout') }}
- 								</a>
- 								<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
- 									@csrf
- 								</form>
- 							</li>
+	<!-- Fonts -->
+	<link rel="dns-prefetch" href="//fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
- 						</ul>
+	<!-- Usando Vite -->
+	@vite(['resources/js/app.js'])
+</head>
 
- 					</div>
- 				</nav>
+<body>
+	<div id="app">
 
- 				<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
- 					@yield('content')
- 				</main>
- 			</div>
- 		</div>
+		<div class="container-fluid vh-100">
+			<div class="row h-100">
+				<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
+					<div class="position-sticky pt-3">
+						<ul class="nav flex-column">
 
- 	</div>
- </body>
+							<li class="nav-item">
+								<a class="nav-link text-white" href="/">
+									<i class="fa-solid fa-home-alt fa-lg fa-fw"></i> Home
+								</a>
+							</li>
 
- </html>
+							<li class="nav-item">
+								<a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}" href="{{route('admin.dashboard')}}">
+									<i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
+								</a>
+							</li>
+
+							<li class="nav-item">
+								<a class="nav-link text-white" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+									<i class="fa-solid fa-sign-out-alt fa-lg fa-fw"></i> {{ __('Logout') }}
+								</a>
+								<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+									@csrf
+								</form>
+							</li>
+
+						</ul>
+
+					</div>
+				</nav>
+
+				<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+					@yield('content')
+				</main>
+			</div>
+		</div>
+
+	</div>
+</body>
+
+</html>
