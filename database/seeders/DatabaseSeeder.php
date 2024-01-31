30a79fa1a741a1f820a150084f0c05446bfa2bb2
Ilaria Salvati <ilariasalvati00@gmail.com>
2024-01-31 16:45:27 +0100

    risolto errore nel seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,19 +26,19 @@ class DatabaseSeeder extends Seeder
 
         $movie2= new Movie();
         $movie2->title = "Titanic";
-        $movie1->description = "Il transatlantico Titanic, considerato un gioiello tecnologico ed il più lussuoso piroscafo da crociera mai realizzato, salpa dall'Inghilterra il dieci aprile del 1912 con oltre 1500 passeggeri a bordo per il suo viaggio inaugurale. I viaggiatori sono collocati in tre classi, riflesso delle differenze sociali.";
-        $movie1->date = Carbon::create(1998, 1, 16);
-        $movie1->duration = 210;
-        $movie1->poster_image = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSZHKcyuoqlsdtSxgAvAfp9XyHrFDKLp-7l4OAVznw7BZViQGk0";
-        $movie1->save();
+        $movie2->description = "Il transatlantico Titanic, considerato un gioiello tecnologico ed il più lussuoso piroscafo da crociera mai realizzato, salpa dall'Inghilterra il dieci aprile del 1912 con oltre 1500 passeggeri a bordo per il suo viaggio inaugurale. I viaggiatori sono collocati in tre classi, riflesso delle differenze sociali.";
+        $movie2->date = Carbon::create(1998, 1, 16);
+        $movie2->duration = 210;
+        $movie2->poster_image = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSZHKcyuoqlsdtSxgAvAfp9XyHrFDKLp-7l4OAVznw7BZViQGk0";
+        $movie2->save();
 
-        $movie2= new Movie();
-        $movie2->title = "Chicago";
-        $movie1->description = "In una prigione degli anni Venti due ragazze vengono aiutate da un avvocato senza scrupoli. Una volta libere, la loro carriera come ballerine prende il volo.";
-        $movie1->date = Carbon::create(2002, 12, 27);
-        $movie1->duration = 113;
-        $movie1->poster_image = "https://pad.mymovies.it/filmclub/2003/01/020/locandina.jpg";
-        $movie1->save();
+        $movie3= new Movie();
+        $movie3->title = "Chicago";
+        $movie3->description = "In una prigione degli anni Venti due ragazze vengono aiutate da un avvocato senza scrupoli. Una volta libere, la loro carriera come ballerine prende il volo.";
+        $movie3->date = Carbon::create(2002, 12, 27);
+        $movie3->duration = 113;
+        $movie3->poster_image = "https://pad.mymovies.it/filmclub/2003/01/020/locandina.jpg";
+        $movie3->save();
 
 
     }
